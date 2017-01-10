a1a0036e2857518663b739d2e86d92bf169dcb2f
Sreenath P G <sreenathg@qburst.com>
2017-01-10 18:49:48 +0530

    pending sections completed

--- js/script.js
+++ js/script.js
@@ -1,19 +1,25 @@
 $(document).ready(function() {
-	var header_size = $('header').height();
+	
+    var header_size = $('header').height();
+    
+   
+ $('.menu_btn').on('click', function(e) {
+ 		e.stopPropagation();
+ 		e.preventDefault();
+        $('.nav_bar').toggleClass('nav_bar_expand');
+    });
+ $(document).click(function(){
+ 		$('.nav_bar').removeClass('nav_bar_expand');
+ });
 
-	$('.menu_btn').on('click', function(e) {
-		e.preventDefault();
-		$('.nav_bar').toggleClass('nav_bar_expand');
-	});
 
+    $('.nav_links').on('click', function(e) {
+        e.preventDefault();
+        var section_id = "#" + $(this).data("jumpto");
+        $('html, body').animate({
+            scrollTop: $(section_id).offset().top - header_size
+        }, 'slow');
+    });
+    $('div.panorama').paver();
 
-
-	$('.nav_links').on('click', function(e) {
-		e.preventDefault();
-		var section_id= "#"+$(this).data("jumpto");
-		$('html, body').animate({
-			scrollTop: $(section_id).offset().top - header_size
-		}, 'slow');
-	});
-	$('div.panorama').paver();
 });
