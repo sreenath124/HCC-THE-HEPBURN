1e7766181cfe2abbe86fc7ac5cd0f46a253e5cc3
Sreenath P G <sreenathg@qburst.com>
2017-01-10 09:46:50 +0530

    slider and panorama added

---
+++ js/script.js
@@ -0,0 +1,19 @@
+$(document).ready(function() {
+	var header_size = $('header').height();
+
+	$('.menu_btn').on('click', function(e) {
+		e.preventDefault();
+		$('.nav_bar').toggleClass('nav_bar_expand');
+	});
+
+
+
+	$('.nav_links').on('click', function(e) {
+		e.preventDefault();
+		var section_id= "#"+$(this).data("jumpto");
+		$('html, body').animate({
+			scrollTop: $(section_id).offset().top - header_size
+		}, 'slow');
+	});
+	$('div.panorama').paver();
+});
